4479aa2ab4cc55d7a2ce6afe7f4dcd89e5f8b538
Hazem Mohamed <hazemhkcyy@gmail.com>
2025-07-05 20:46:30 +0300

    - add sliding animation to splash view

--- lib/features/splash/ui/splash_screen.dart
+++ lib/features/splash/ui/splash_screen.dart
@@ -6,8 +6,6 @@ class SplashScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: const SplashScreenView(),
-    );
+    return Scaffold(body: const SplashScreenView());
   }
 }
--- lib/features/splash/ui/widgets/splash_screen_view.dart
+++ lib/features/splash/ui/widgets/splash_screen_view.dart
@@ -1,9 +1,40 @@
+import 'package:bookly_app/features/splash/ui/widgets/sliding_text.dart';
 import 'package:flutter/material.dart';
 
 import '../../../../core/helpers/assets.dart';
 
-class SplashScreenView extends StatelessWidget {
+class SplashScreenView extends StatefulWidget {
   const SplashScreenView({super.key});
+
+  @override
+  State<SplashScreenView> createState() => _SplashScreenViewState();
+}
+
+class _SplashScreenViewState extends State<SplashScreenView>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<Offset> slidingAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 1),
+    );
+    slidingAnimation = Tween<Offset>(
+      begin: const Offset(0, 2),
+      end: const Offset(0, 0),
+    ).animate(animationController);
+    animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    animationController.dispose();
+
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,15 +43,10 @@ class SplashScreenView extends StatelessWidget {
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
         Image.asset(AssetsData.logo),
-        const Text(
-          'Read Free Books',
-          textAlign: TextAlign.center,
-          style: TextStyle(
-            fontSize: 25,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
+        SlidingText(slidingAnimation: slidingAnimation),
       ],
     );
   }
 }
+
+
